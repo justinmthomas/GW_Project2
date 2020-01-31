1093ecd1cec8deb47ff8abdeabefe9151446f4f8
Woman <52986268+tateha26@users.noreply.github.com>
2020-01-30 20:33:22 -0500

    Adding survey_results schema

---
+++ index.sql
@@ -0,0 +1,10 @@
+USE survey_results;
+
+CREATE TABLE survey_results (
+	attempt INT (255),
+    Question INT (40),
+    Data_Type VARCHAR (40),
+    Chart_Type varchar (40),
+    Correct INT (10)
+)
+
