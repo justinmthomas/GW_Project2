8a802b480e4568b75c5d81b5da26d4438112dac7
Woman <52986268+tateha26@users.noreply.github.com>
2020-02-01 15:05:57 -0500

    Adding sql to plotly draft

--- index.sql
+++ index.sql
@@ -1,10 +1,14 @@
 USE survey_results;
 
 CREATE TABLE survey_results (
-	attempt INT (255),
+	id INT (6) AUTO_INCREMENT PRIMARY KEY,
+    attempt INT (255),
     Question INT (40),
     Data_Type VARCHAR (40),
     Chart_Type varchar (40),
-    Correct INT (10)
+    Correct INT (10),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
+
+DROP TABLE survey_results;
 
