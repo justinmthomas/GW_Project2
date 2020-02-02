0f4c017bc8af11deb96a76a8ac324e8d12a3292b
Justin Thomas <justint@manyroads.org>
2020-02-02 13:58:36 -0500

    updated app and added Heroku procfile and requirements

--- config.py
+++
@@ -1,6 +0,0 @@
-# Add your API key
-remote_db_endpoint = 'tm-3.cofufb4l3rai.us-east-2.rds.amazonaws.com'
-remote_db_port = 3306
-remote_gwsis_dbname = 'survey_results'
-remote_gwsis_dbuser = 'admin'
-remote_gwsis_dbpwd = 'YA*Q3BD!'
