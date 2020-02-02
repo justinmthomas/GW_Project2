6db9be855a5f4dd9c416b932d618307d6c569a14
Justin Thomas <justint@manyroads.org>
2020-02-02 11:26:54 -0500

    updated app.py

--- Survey_script/quiz.js
+++ Survey_script/quiz.js
@@ -296,7 +296,10 @@ survey
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "http://127.0.0.1:5000/postjson");
         xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-        xhr.send(JSON.stringify(result.data));
+        xhr.send('{"Survey_ID":"' + String.random(50) + '","result":' + JSON.stringify(newData) + '}');
+        // xhr.send(JSON.stringify('{"Survey_ID":"' + String.random(50) + '","result":' + JSON.stringify(newData) + '}'));
+        // // #xhr.send(JSON.stringify('{"Survey_ID":"' + String.random(50) + '","result:"' + JSON.stringify(newData) + '}'));
+        // // // xhr.send(JSON.stringify(result.data));
     });
 
 //Create showdown mardown converter
