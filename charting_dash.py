c09e0bbc636243933e729bc62db426693ea1b792
aisaacson1 <50605527+aisaacson1@users.noreply.github.com>
2020-03-04 18:48:26 -0500

    updating dash files

--- charting_dash.py
+++ charting_dash.py
@@ -9,8 +9,11 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import dash_bootstrap_components as dbc
 
 import pandas as pd
+
+import chart_library as cl
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -31,7 +34,7 @@ app.layout = html.Div([
             html.A('Select Files')
         ]),
         style={
-            'width': '100%',
+            'width': '99%',
             'height': '60px',
             'lineHeight': '60px',
             'borderWidth': '1px',
@@ -43,9 +46,18 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
+
+    # html.Label('Select columns to graph:'),
+
+    # dcc.Checklist(id='output-column-upload',
+    #              labelStyle = dict(display='inline')),
+
     # dcc.Graph(id='Mygraph'),
-    html.Div(id='output-data-upload')
+    html.Div(id='output-data-upload'),
+    html.Div(id='output-column-selected', style={'display': 'none'}),
+    # html.Div(id='datatable-interactivity-container')
 ])
+
 
 def parse_data(contents, filename):
     content_type, content_string = contents.split(',')
@@ -71,30 +83,18 @@ def parse_data(contents, filename):
 
     return df
 
+@app.callback(
+                # Output('output-data-upload', 'children'),
 
-# @app.callback(Output('Mygraph', 'figure'),
-#             [
-#                 Input('upload-data', 'contents'),
-#                 Input('upload-data', 'filename')
-#             ])
-# def update_graph(contents, filename):
-#     fig = {
-#         'layout': go.Layout(
-#             plot_bgcolor=colors["graphBackground"],
-#             paper_bgcolor=colors["graphBackground"])
-#     }
+            [
+                Output('output-data-upload', 'children'),
+                Output('output-column-selected', 'selected_columns')
+                ],
 
-#     if contents:
-#         contents = contents[0]
-#         filename = filename[0]
-#         df = parse_data(contents, filename)
-#         df = df.set_index(df.columns[0])
-#         fig['data'] = df.iplot(asFigure=True, kind='scatter', mode='lines+markers', size=1)
-
-
-#     return fig
-
-@app.callback(Output('output-data-upload', 'children'),
+            # [
+            #     Output('output-data-upload', 'children'),
+            #     Output('output-column-upload', 'options')
+            #     ],
             [
                 Input('upload-data', 'contents'),
                 Input('upload-data', 'filename')
@@ -106,25 +106,114 @@ def update_table(contents, filename):
         contents = contents[0]
         filename = filename[0]
         df = parse_data(contents, filename)
-
+        column_head = [{'label': i, 'value': i} for i in df.columns]
         table = html.Div([
+            html.H5('Select columns to graph:'),
+            dcc.Checklist(options=column_head,
+            labelStyle = dict(display='inline')),
             html.H5(filename),
             dash_table.DataTable(
+                id='user_data',
                 data=df.to_dict('rows'),
-                columns=[{'name': i, 'id': i} for i in df.columns]
+                columns=[{'name': i, 'id': i, "selectable": True} for i in df.columns],
+                fixed_rows={ 'headers': True, 'data': 0 },
+                column_selectable = 'multi',
+                selected_columns=[],
+                # filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                page_action="native",
+                page_size= 20,
+                style_table={
+                    'maxHeight': '500px',
+                    # 'overflowY': 'scroll',
+                    'overflowX': 'scroll',
+                },
+                style_cell_conditional=create_conditional_style(df)
             ),
-            html.Hr(),
-            html.Div('Raw Content'),
-            html.Pre(contents[0:200] + '...', style={
-                'whiteSpace': 'pre-wrap',
-                'wordBreak': 'break-all'
-            })
         ])
 
+    #     column_head = [{'label': i, 'value': i} for i in df.columns]
+    # return table, column_head
     return table
 
+def create_conditional_style(df):
+    style=[]
+    for col in df.columns:
+        name_length = len(col)
+        pixel = 50 + round(name_length*1)
+        pixel = str(pixel) + "px"
+        style.append({'if': {'column_id': col}, 'minWidth': pixel})
+
+    return style
+
+# @app.callback(
+#                 Output('output-column-upload', 'value')
+#                 ,
+#                 Input('output-column-upload', 'options')
+#             )
+# def set_checks_value(available_options):
+#     return available_options[0]['value']
+
+# @app.callback(
+#                 Output('output-column-upload', 'options')
+#                 ,
+#                 Input('output-data-upload', 'children')
+#             )
+# def set_checks_value(available_options):
+#     return available_options[0]['value']
 
 
+# @app.callback(
+#     Output('datatable-interactivity-container', "children"),
+#     [Input('user_data', 'selected_columns'),
+#      Input('output-data-upload', "derived_virtual_selected_columns")])
+# def update_graphs(columns, derived_virtual_selected_columns):
+#     # When the table is first rendered, `derived_virtual_data` and
+#     # `derived_virtual_selected_rows` will be `None`. This is due to an
+#     # idiosyncracy in Dash (unsupplied properties are always None and Dash
+#     # calls the dependent callbacks when the component is first rendered).
+#     # So, if `rows` is `None`, then the component was just rendered
+#     # and its value will be the same as the component's dataframe.
+#     # Instead of setting `None` in here, you could also set
+#     # `derived_virtual_data=df.to_rows('dict')` when you initialize
+#     # the component.
+#     if derived_virtual_selected_columns is None:
+#         derived_virtual_selected_columns = []
+
+#     dff = df if columns is None else pd.DataFrame(columns)
+
+#     colors = ['#7FDBFF' if i in derived_virtual_selected_columns else '#0074D9'
+#               for i in range(len(dff))]
+
+#     return [
+#         dcc.Graph(
+#             id=column,
+#             figure={
+#                 "data": [
+#                     {
+#                         "x": dff["Chart_Type"],
+#                         "y": dff[column],
+#                         "type": "bar",
+#                         "marker": {"color": colors},
+#                     }
+#                 ],
+#                 "layout": {
+#                     "xaxis": {"automargin": True},
+#                     "yaxis": {
+#                         "automargin": True,
+#                         "title": {"text": column}
+#                     },
+#                     "height": 250,
+#                     "margin": {"t": 10, "l": 10, "r": 10},
+#                 },
+#             },
+#         )
+#         # check if column exists - user may have deleted it
+# #         # If `column.deletable=False`, then you don't
+# #         # need to do this check.
+# #     #     for column in ["pop", "lifeExp", "gdpPercap"] if column in dff
+#     ]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
